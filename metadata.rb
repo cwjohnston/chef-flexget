f0e492d863e251cbc2087bccaaac99ac375c5b40
Cameron Johnston <cameron@rootdown.net>
2015-03-05 09:22:42 -0700

    bump to 0.1.1 for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rootdown.net'
 license 'Apache 2.0'
 description 'Installs/Configures flexget'
 long_description 'Installs/Configures flexget'
-version '0.1.0'
+version '0.1.1'
 
 depends 'python'
 depends 'runit'
